46ca358a480ccceee82bdb6e0fb729e46fc67328
Pierre Chemin <90683303+PietroCookie@users.noreply.github.com>
2024-06-13 22:18:45 +0200

    Ajout de 2 scripts
    Un script pour ajouter les full droits sur un ensemble de dossiers et leurs sous-dossiers à un utilisateur (ou groupe de l'AD) spécifique.
    Deuxième script pour extraire tous les utilisateurs d'un AD avec les dates du dernier changement de mot de passe, dates de dernières connexions.

---
+++ Changements_droits_dossier_en_masse.ps1
@@ -0,0 +1,18 @@
+﻿# Chemin du dossier dont on modifie les droits de tous les sous-répertoires
+$path = "PATH"
+# Nom du compte à qui on rajoute les droits
+$accountNewAccess = "USER/OU"
+
+Function ModifyAccess($folder) {
+    $securityFolder = Get-Acl -Path $folder
+    $newRulesAccess = New-Object System.Security.AccessControl.FileSystemAccessRule($accountNewAccess, "FullControl", "ContainerInherit,ObjectInherit", "None", "Allow")
+
+    $securityFolder.AddAccessRule($newRulesAccess)
+    Set-Acl -Path $folder -AclObject $securityFolder
+
+    Write-Host "Les droits sur $folder pour $accountNewAccess ont été ajouté"
+}
+
+Get-ChildItem -Path $path -Directory -Recurse | ForEach-Object {
+    ModifyAccess($_.FullName)
+}
---
+++ Extractions_AD_Users.ps1
@@ -0,0 +1,39 @@
+﻿# Spécifier le chemin de l'OU que vous souhaitez extraire
+$ouPath = "OU=ORGANIZATIONAL_UTILS,DC=DOMAINE.LOCAL,DC=ad"
+
+# Obtenir tous les utilisateurs de l'OU spécifiée avec les champs souhaités
+$users = Get-ADUser -Filter * -SearchBase $ouPath -Properties PasswordLastSet,LastLogonDate,Enabled,lastLogon,MemberOf
+
+# Initialiser une collection pour stocker les détails de chaque utilisateur
+$userDetails = @()
+
+# Parcourir chaque utilisateur et extraire les détails requis
+foreach ($user in $users) {
+  $lastPasswordSet = $user.PasswordLastSet
+  $lastLogonDate = $user.LastLogonDate
+
+  $lastLogon = [DateTime]::FromFileTimeUtc($user.lastLogon)
+
+  $estMembrePSOUsers = $user.MemberOf -contains (Get-ADGroup -Filter { Name -eq "GG_PSO_users" }).DistinguishedName
+   
+  # Vérifier si le compte est actif
+  if ($user.Enabled) {
+    $accountStatus = "Actif"
+  } else {
+    $accountStatus = "Inactif"
+  }
+
+  # Ajouter les détails de l'utilisateur à la collection
+  $userDetails += New-Object PSObject -Property @{
+    "Nom du compte" = $user.SamAccountName
+    "Date du dernier changement de mot de passe" = $lastPasswordSet
+    "Compte actif" = $accountStatus
+    "Date du dernier login (lastLogonTimestamp)" = $lastLogonDate
+    "Date du dernier login (lastLogon)" = $lastLogon
+    "Membre du groupe GG_PSO_users" = $estMembrePSOUsers
+    
+  }
+}
+
+# Exporter les détails des utilisateurs dans un fichier CSV
+$userDetails | Export-Csv -Path "C:\temp\User_extract.csv" -NoTypeInformation
